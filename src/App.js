984ca7cfa5e5153170aaad01209bb385f2d2cd6c
nassirou <nassirou9993@gmail.com>
2023-07-22 10:59:46 +0100

    footer party

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Order from './components/Order';
 
 function App() {
   return (
-    <div className='min-h-screen flex justify-center items-center bg-green-100'>
+    <div className='min-h-screen flex flex-col justify-center items-center bg-green-100'>
       <Order/>
+      <footer className="absolute bottom-3">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className='text-blue-400'>Frontend Mentor</a>.Coded by <a href="#" className='text-blue-500'>Nassirou</a>.
+      </footer>
     </div>
   );
 }
